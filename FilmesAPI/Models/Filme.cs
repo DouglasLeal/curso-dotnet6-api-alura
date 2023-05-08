041607a4dbb415cf543ef6bee1e0acdba0036e19
Douglas Leal <douglas.leal@live.com>
2023-05-08 09:41:59 -0300

    Utilizando DataAnnotation em Filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,23 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [DisplayName("Título")]
         public string? Titulo { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [DisplayName("Gênero")]
         public string? Genero { get; set; }
+
+        [Required(ErrorMessage = "A {0} é obrigatório.")]
+        [Range(70, 600, ErrorMessage = "A {0} deve ser entre 30 e 600 minutos.")]
+        [DisplayName("Duração")]
         public int Duracao { get; set; }
     }
 }
