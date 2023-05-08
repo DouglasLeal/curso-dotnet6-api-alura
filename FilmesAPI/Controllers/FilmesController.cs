bf9d5f68a81d9ab82e4c44fe5f2a28198773b0da
Douglas Leal <douglas.leal@live.com>
2023-05-08 09:22:18 -0300

    recebendo dados via post

---
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        private static List<Filme> _filmes = new List<Filme>();
+
+        [HttpPost]
+        public void Adicionar([FromBody]Filme filme)
+        {
+            _filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
