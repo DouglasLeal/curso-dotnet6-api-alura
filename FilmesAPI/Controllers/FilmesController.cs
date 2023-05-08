a8f35bfda5671a95c0be230e1ab79f9702356e83
Douglas Leal <douglas.leal@live.com>
2023-05-08 11:46:56 -0300

    Persistindo dados com DB

--- FilmesAPI/Controllers/FilmesController.cs
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,18 @@ namespace FilmesAPI.Controllers
     [ApiController]
     public class FilmesController : ControllerBase
     {
-        private static List<Filme> _filmes = new List<Filme>();
-        private static int id = 0;
+        private FilmeContext _context;
+
+        public FilmesController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult Adicionar([FromBody]Filme filme)
         {
-            id++;
-            filme.Id = id;
-            _filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
 
             return CreatedAtAction(nameof(BuscarPorId), new { id = filme.Id }, filme);
         }
@@ -24,7 +28,7 @@ namespace FilmesAPI.Controllers
         [HttpGet]
         public IActionResult Listar([FromQuery] int skip = 0, [FromQuery] int take = 10)
         {
-            var filmes = _filmes.Skip(skip).Take(take);
+            var filmes = _context.Filmes.Skip(skip).Take(take);
 
             return Ok(filmes);
         }
@@ -32,7 +36,7 @@ namespace FilmesAPI.Controllers
         [HttpGet("{id}")]
         public IActionResult BuscarPorId(int id)
         {
-            var filme = _filmes.FirstOrDefault(filme => filme.Id == id);
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if (filme == null) return NotFound();
 
