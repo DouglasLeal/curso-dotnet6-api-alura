5b771569a8f8de5baf28e5786b37b19cda20c5b5
Douglas Leal <douglas.leal@live.com>
2023-05-09 11:21:41 -0300

    classes DTO de cinema criadas

---
+++ FilmesAPI/Data/Dtos/Cinema/CreateCinemaDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos.Cinema
+{
+    public class CreateCinemaDto
+    {
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        public string Nome { get; set; }
+    }
+}
